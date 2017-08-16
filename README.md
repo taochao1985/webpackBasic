502901c011c4dd42c202d2a42e73e6646ee90281
taochao <taochao@smm.cn>
2017-08-16 09:40:27 +0800

    first version

---
+++ README.md
@@ -0,0 +1,4 @@
+# webpack2basics4
+webpack-scss-babel-es6
+
+I modified wedpack-dev.server.js to port (9091) localhost:9091
---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+import 'jquery';
+import '../css/main.scss';
+import '../users.html';
+
+import { RandomGenerator } from './random-generator';
+
+const outputParagraph = $('#outputParagraph');
+
+const outputRandomInt = () => {
+    outputParagraph.text(RandomGenerator.randomInteger());
+};
+
+const outputRandomRange = () => {
+    outputParagraph.text(RandomGenerator.randomRange(1, 500));
+};
+
+const buttonRndInt = jQuery('#randomInt');
+const buttonRndRange = document.querySelector('#randomRange');
+
+buttonRndInt.click(outputRandomInt);
+buttonRndRange.addEventListener('click', outputRandomRange);
